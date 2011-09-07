8b292e085be76ff60c88797daa36a1e01b2a2f37
Sergio Pascual <sergiopr@fis.ucm.es>
2011-09-07 19:11:36 +0200

    Adding a Recipe class

--- numina/recipes/megara/recipe1.py
+++ numina/recipes/megara/recipe1.py
@@ -4,3 +4,14 @@ __all__ = ['run']
 
 def run(rb, pp, cp):
     return None
+
+class Recipe:
+    def __init__(self, pp, cp):
+        pass
+
+    @classmethod
+    def requires(cls):
+        return []
+
+    def run(self, rb):
+        return 0
