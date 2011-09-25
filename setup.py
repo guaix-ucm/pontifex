1da4ad85196f8c1933329450437e0dd2e6b7bb21
Sergio Pascual <sergiopr@fis.ucm.es>
2011-09-26 00:53:56 +0200

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pontifex',
+      version='0.4.0',
+      author='Sergio Pascual',
+      author_email='sergiopr@fis.ucm.es',
+      url='http://guaix.fis.ucm.es/~spr',
+      license='GPLv3',
+      description='Pontifex automatic reduction system',
+      packages=['pontifex', 'pontifex.model'],
+      scripts=['scripts/pontifex-server.py', 'scripts/pontifex-host.py'],
+      classifiers=[
+                   "Programming Language :: Python",
+                   'Development Status :: 3 - Alpha',
+                   "Environment :: Other Environment",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: GNU General Public License (GPL)",
+                   "Operating System :: OS Independent",
+                   "Topic :: Scientific/Engineering :: Astronomy",
+                   ],
+)
