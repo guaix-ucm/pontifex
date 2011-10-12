c93655c422e34b3f0c4c45b32e86fc745bd1f9fc
Sergio Pascual <sergiopr@fis.ucm.es>
2011-10-12 23:14:22 +0200

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='pontifex',
-      version='0.4.0',
+      version='0.4.1',
       author='Sergio Pascual',
       author_email='sergiopr@fis.ucm.es',
       url='http://guaix.fis.ucm.es/~spr',
