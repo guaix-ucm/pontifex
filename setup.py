b51041b856e53c8801b06e0260934dff97ac5efe
Sergio Pascual <sergiopr@fis.ucm.es>
2011-10-18 17:31:18 +0200

    Missing "," in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='pontifex',
                 'clodia', 'clodia.recipes'
                 ],
       scripts=['scripts/pontifex-server.py', 'scripts/pontifex-host.py'],
-      install_requires=['numina']
+      install_requires=['numina'],
       classifiers=[
                    "Programming Language :: Python",
                    'Development Status :: 3 - Alpha',
